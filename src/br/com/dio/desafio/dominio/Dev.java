2aea748d9551709207864016d1536962b658ffc0
Maria Augusta <mamb2@cin.ufpe.br>
2022-08-04 09:57:09 -0300

    heranca e polimorfismo 2

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,56 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscrito = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosFinalizados = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){}
+
+    public void progredir(){}
+
+    public void calcularTotalXp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscrito() {
+        return conteudosInscrito;
+    }
+
+    public void setConteudosInscrito(Set<Conteudo> conteudosInscrito) {
+        this.conteudosInscrito = conteudosInscrito;
+    }
+
+    public Set<Conteudo> getConteudosFinalizados() {
+        return conteudosFinalizados;
+    }
+
+    public void setConteudosFinalizados(Set<Conteudo> conteudosFinalizados) {
+        this.conteudosFinalizados = conteudosFinalizados;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) &&
+                Objects.equals(conteudosInscrito, dev.conteudosInscrito) &&
+                Objects.equals(conteudosFinalizados, dev.conteudosFinalizados);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscrito, conteudosFinalizados);
+    }
+}
